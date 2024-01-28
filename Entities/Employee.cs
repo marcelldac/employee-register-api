b67f97cfec39e4172a5d7e0d1e0206f30ae7e4a1
marcelldac <dacmarcell@gmail.com>
2024-01-28 08:27:55 -0300

    feat: employee model

---
+++ Entities/Employee.cs
@@ -0,0 +1,11 @@
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string BranchLine { get; set; }
+    public string Email { get; set; }
+    public string Department { get; set; }
+    public decimal Salary { get; set; }
+    public DateTime AdmissionDate { get; set; }
+}
